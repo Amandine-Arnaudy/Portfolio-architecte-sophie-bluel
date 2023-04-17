ff42c7bcd87f10074b35764df8c0f8b5038ff9c1
Amandine <amandine.arnaudy11@gmail.com>
2023-04-17 12:54:53 +0200

    Log In and log Out functional

--- FrontEnd/js/script.js
+++ FrontEnd/js/script.js
@@ -53,13 +53,15 @@ async function categoriesButtons() {
             }
         });
 
+    // H2 : Mes projets
     const portfolio = document.querySelector('#portfolio');
     const myProject = document.createElement('h2');
     myProject.textContent = 'Mes Projets';
 
     if (window.sessionStorage.getItem('token') === null) {
         const categories = await response.json();
-
+         
+        // Filtres Catégories
         const filtersButtons = document.createElement('div');
         filtersButtons.classList.add('categories');
         const categoriesButtons = document.createElement('button');
@@ -68,13 +70,14 @@ async function categoriesButtons() {
         filtersButtons.appendChild(categoriesButtons);
         portfolio.prepend(filtersButtons);
         portfolio.prepend(myProject);
+
         for (let categorie of categories) {
-            console.log(categorie);
+            //console.log(categorie);
             const button = document.createElement('button');
             button.setAttribute('class', 'btn');
             button.setAttribute('data-category-id', `${categorie.id}`);
             button.textContent = `${categorie.name}`;
-            console.log(button.dataset.categoryId);
+            //console.log(button.dataset.categoryId);
             filtersButtons.appendChild(button);
         }
 
@@ -83,8 +86,16 @@ async function categoriesButtons() {
             buttons[i].addEventListener('click', showCategories);
         }
     } else {
-        //const works = JSON.parse(window.localStorage.getItem('works'));
-
+        logout();
+        topBlackMenu();
+        portfolio.prepend(myProject);
+        const aLink = elementGenerator('a', undefined, ['href=#']);
+        const iElement = elementGenerator('i', undefined, ['class=fa-regular fa-pen-to-square']);
+        const spanElement = elementGenerator('span', 'modifier', []);
+        aLink.appendChild(iElement);
+        aLink.appendChild(spanElement);
+        myProject.appendChild(aLink);
+        modifyButton();
     }
 }
 
@@ -111,8 +122,55 @@ function generateGallery(works) {
     }
 }
 
-
-
 allWorks();
 categoriesButtons();
 
+// Id Login - Logout
+function logout() {
+    const logoutElement = document.getElementById("logout");
+    logoutElement.textContent = "logout";
+    logoutElement.addEventListener('click', () => {
+        window.sessionStorage.removeItem('token');
+        alert('Vous avez été déconnecté.');
+    });
+}
+
+// Générateur d'éléments DOM
+function elementGenerator(type, text, attributes = []) {
+    let element = document.createElement(type);
+    if (text) {
+        element.textContent = text;
+    }
+    attributes.forEach(attribute => {
+        const [name, value] = attribute.split('=');
+        element.setAttribute(name, value);
+    });
+    return element;
+}
+
+// Top édition noir
+function topBlackMenu() {
+    const div = elementGenerator('div', undefined, ['class=publish']);
+    const modifyLink = elementGenerator('a', undefined, ['href=#']);
+    const span = elementGenerator('span', 'Mode édition', []);
+    const button = elementGenerator('button', 'publier les changements', []);
+    const icon = elementGenerator('i', undefined, ['class=fa-regular fa-pen-to-square']);
+    div.appendChild(modifyLink);
+    document.querySelector('body').prepend(div);
+    modifyLink.appendChild(icon);
+    modifyLink.appendChild(span);
+    div.appendChild(button);
+}
+
+// Lien Modifier 
+function modifyButton() {
+    const introductionSection = document.querySelector("#introduction figure");
+    const divElement = document.createElement('div');
+    const aLink = elementGenerator('a', undefined, ['href=#']);
+    const iElement = elementGenerator('i', undefined, ['class=fa-regular fa-pen-to-square']);
+    const spanElement = elementGenerator('span', 'modifier', []);
+    aLink.appendChild(iElement);
+    aLink.appendChild(spanElement);
+    divElement.appendChild(aLink);
+    introductionSection.appendChild(divElement);
+}
